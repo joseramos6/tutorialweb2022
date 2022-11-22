c927d63645acfdcc198d48d5149ee124d94866e8
Jose Ramos <jlramos5@misena.edu.co>
2022-11-22 15:54:12 -0500

    Initial commit

---
+++ Config/Config.php
@@ -0,0 +1,26 @@
+<?php
+
+const BASE_URL = "http://localhost/tutorialweb/tutorialweb2022";
+
+//Datos de conexión a Base de Datos
+const DB_HOST = "localhost";
+const DB_NAME = "tutorialweb";
+const DB_USER = "root";
+const DB_PASSWORD = "";
+const DB_CHARSET = "utf8";
+
+//Zona horaria
+date_default_timezone_set('America/Bogota');
+
+//Deliminadores decimal y millar Ej. 24,1989.00
+const SPD = ".";
+const SPM = ",";
+
+//Simbolo de moneda
+const SMONEY = "$";
+
+//Datos envio de correo
+const NOMBRE_REMITENTE = "Tienda Virtual";
+const EMAIL_REMITENTE = "jose@ramos.com";
+const NOMBRE_EMPESA = "Tienda Virtual";
+const WEB_EMPRESA = "www.miempresa.com.co";
